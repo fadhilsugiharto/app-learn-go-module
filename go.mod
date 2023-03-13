43d3e84e27a37d6c97f484b08559054749857e9d
Fadhil Sugiharto <hanun.sugiharto@pactindo.com>
2023-03-13 11:18:36 +0700

    import dependency from learn-go-module/v2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/fadhilsugiharto/app-learn-go-module
 
 go 1.19
 
-require github.com/fadhilsugiharto/learn-go-module v0.0.0-20230309074844-952c310e606d // indirect
+require github.com/fadhilsugiharto/learn-go-module/v2 v2.0.0-20230313040929-ba41c2c5fd4a
--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	learn_go_module "github.com/fadhilsugiharto/learn-go-module"
+	learn_go_module "github.com/fadhilsugiharto/learn-go-module/v2"
 )
 
 func main() {
-	fmt.Print(learn_go_module.Hello())
+	fmt.Println(learn_go_module.Hello("Fadhil"))
 }
